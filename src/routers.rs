53eb1d0d9e5bba445f09a6bd2b25ca9f403a7cbd
Serhii Skumin <serhiiskumin@gmail.com>
2025-12-04 14:51:56 +0100

    Add json

--- src/routers.rs
+++ src/routers.rs
@@ -1,4 +1,27 @@
-use axum::{routing::get, Router, extract::Path};
+use axum::{Json, routing::{get, post}, Router, extract::Path};
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize)]
+pub struct Person {
+    name: String,
+    age: u8,
+}
+
+#[derive(Serialize)]
+pub struct PersonResponse {
+    status: String,
+    name: String,
+    age: u8,
+}
+
+pub async fn add_person(Json(person): Json<Person>) -> Json<PersonResponse> {
+    let response = PersonResponse {
+        status: "received".to_string(),
+        name: person.name,
+        age: person.age,
+    };
+    Json(response)
+}
 
 pub async fn home() -> &'static str {
     "Welcome to the Home Page!"
@@ -12,4 +35,5 @@ pub fn create_router() -> Router {
     Router::new()
         .route("/", get(home))
         .route("/hello/{name}", get(hello))
+        .route("/person", post(add_person))
 }
